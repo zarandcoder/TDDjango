f2433b46a7c7f738e4e513bb42ef09888ca30738
Vadim Midav <vzaripov@gmx.de>
2019-10-13 22:58:28 +0200

    Initial commit

--- TDDjango/settings.py
+++ TDDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#q)fb9r5(@k$lds4(i^j2uym5aymlwot$a-)6%j&8+4iy52mg('
+SECRET_KEY = os.get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
